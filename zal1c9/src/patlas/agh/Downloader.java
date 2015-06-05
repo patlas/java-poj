cdda2a175911121a5665f832482b529cc1621120
PatLas <plas@student.agh.edu.pl>
2015-06-05 12:24:12 +0200

    Add Downloader.java class

---
+++ zal1c9/src/patlas/agh/Downloader.java
@@ -0,0 +1,145 @@
+package patlas.agh;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
+public class Downloader {
+
+	private URL url;
+	private File file;
+	public int numtry;
+	public int timeout;
+	private String fName;
+	
+	public Boolean isDownloaded = false;
+	private String dir= "web_pages/";
+	
+	
+	private static String MD5string(String str) //tworzenie nazw plików je¿eli takowych nie podano
+	{
+		MessageDigest md5;
+		String ret = null;
+		try {
+			md5 = MessageDigest.getInstance("MD5");
+			md5.update(StandardCharsets.UTF_8.encode(str));
+			ret = String.format("%032x", new BigInteger(1, md5.digest()));
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();//DODAÆ LOGGER!!!
+		}
+		//System.out.println(String.valueOf(Math.abs(str.hashCode())));
+		//Integer x = Integer.valueOf("895487832138352138574", 16);
+		//System.out.println(ret);
+				
+		return String.valueOf(Math.abs(str.hashCode()));//ret;
+	}
+	
+	
+	public Boolean downloadPage()
+	{
+	    InputStream is = null;
+	    BufferedReader br;
+	    BufferedWriter bw = null;
+	    String line;
+	    
+	    if(!file.exists())
+		{
+			try{
+				file.createNewFile();
+			}
+			catch(IOException ioe){
+		         System.out.println("Exception occurred:"); //DODAÆ LOGGER!!!
+		    	 ioe.printStackTrace();
+			}
+		}
+
+	    try {
+	        is = url.openStream();  // throws an IOException
+	        br = new BufferedReader(new InputStreamReader(is));
+	        bw = new BufferedWriter(new FileWriter(file,false));
+	        //PrintWriter writer = new PrintWriter( new FileOutputStream(file, false) );
+	        while ((line = br.readLine()) != null) {
+	            //System.out.println(line);
+	        	bw.write(line);
+	        	bw.newLine();
+	        }
+	    } catch (MalformedURLException mue) {
+	         mue.printStackTrace();
+	    } catch (IOException ioe) {
+	         ioe.printStackTrace();
+	    } finally {
+	        try {
+	            if (is != null) is.close();
+	            if (bw != null) bw.close();
+	        } catch (IOException ioe) {
+	            // nothing to see here
+	        }
+	    }
+	    isDownloaded = true;
+	    return true;
+	}
+	
+	public String getStringUrl()
+	{
+		return url.toString();
+	}
+	
+	public Downloader(String urlString)
+	{
+		fName = "f_"+Downloader.MD5string(urlString)+".html";
+		file = new File(dir+fName);
+		try {
+			url = new URL(urlString);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}				
+	}
+	
+	public Downloader(URL Url)
+	{
+		fName = "f_"+Downloader.MD5string(Url.toString())+".html";
+		file = new File(dir+fName);
+		url = Url;
+	}
+	
+	public Downloader(String urlString, String fileName)
+	{
+		if(fileName.contains("/") == true)
+		{
+			dir = "";	
+			fName = fileName;
+		}
+		else
+		{
+			if(fileName.contains(".html")==true)
+				fName = fileName;
+			else
+				fName = fileName+".html";
+		}
+		
+		file = new File(dir+fName);
+		
+		try {
+			url = new URL(urlString);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+			
+	}
+
+	
+	
+}
--- zal1c9/src/patlas/agh/Test.java
+++ zal1c9/src/patlas/agh/Test.java
@@ -1,25 +1,49 @@
 package patlas.agh;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import patlas.agh.utils.TwoTypeList;
 
 public class Test {
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Preference x = new Preference();
-		Preference y = new Preference("adsfadf", 2);
-		Preference yf = new Preference("Joanna", 22);
-		Preference yfx = new Preference("Patryk", 25);
-		Preference.setTIMEOUT(20);
-		x.setAddr("www.agbc.pl");
-		//System.out.println(x.getAddr());
+//		Preference x = new Preference();
+//		Preference y = new Preference("adsfadf", 2);
+//		Preference yf = new Preference("Joanna", 22);
+//		Preference yfx = new Preference("Patryk", 25);
+//		Preference.setTIMEOUT(20);
+//		x.setAddr("www.agbc.pl");
+//		//System.out.println(x.getAddr());
+//		
+//		Preferences.saveSettings();
+//		Preferences.loadSettings();
+//		
+//		
+//		for(Preference a : Preferences.prefList)
+//			System.out.println(a.getAddr());
 		
-		Preferences.saveSettings();
-		Preferences.loadSettings();
+		//Downloader p = new Downloader("http://pl.kingofsat.net/sat-hb8.php");
 		
-		for(Preference a : Preferences.prefList)
-			System.out.println(a.getAddr());
+		URL url = null;
+		try {
+			url = new URL("http://onet.pl");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		if(url != null)
+			new Downloader(url).downloadPage();
+		
+		
+		new Downloader("http://seguro.pl").downloadPage();
+		new Downloader("http://tme.pl","tme2.html").downloadPage();
+		
+		//System.out.println(p.url.toString());
+		//System.out.println(Downloader.MD5string("patlas"));
 		//System.out.println(Preferences.prefList.get(0).getAddr());
 		//System.out.println(Preferences.prefList.size());
 		//System.out.println(Preferences.prefList.get(Preferences.prefList.size()-2).getAddr());
